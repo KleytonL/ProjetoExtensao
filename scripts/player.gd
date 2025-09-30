609cd9bfceaad81506469627ddda5e3fdab11cd4
KleytonL <kleytonl.vieira@gmail.com>
2025-09-30 17:38:32 -0300

    Adicionando animação e organizando diretório

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
+const DECELERATION = 300.0
 
+@onready var _animation = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+
+	if velocity.x || velocity.y != 0:
+		_animation.play("run")
+	else:
+		_animation.play("idle")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -13,12 +20,13 @@ func _physics_process(delta: float) -> void:
 
 	if direction_x:
 		velocity.x = direction_x * SPEED
+		_animation.flip_h = true if velocity.x < 0 else false 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
 
 	if direction_y:
 		velocity.y = direction_y * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, DECELERATION * delta)
 
 	move_and_slide()
