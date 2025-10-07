318025ab722ebc44be604aa6b7d14bb476f8a628
KleytonL <kleytonl.vieira@gmail.com>
2025-10-07 16:16:20 -0300

    Alterando resources de PNG para SVG

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 75.0
-const DECELERATION = 300.0
+const SPEED = 50.0
+const DECELERATION = 200.0
 
+var can_attack: bool = true
 @onready var _animation = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
 
-	if velocity.x || velocity.y != 0:
+	if Input.is_action_just_pressed("punch"):
+		_animation.play("punch")
+	
+	if velocity.x != 0 || velocity.y != 0:
 		_animation.play("run")
 	else:
 		_animation.play("idle")
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction_x := Input.get_axis("ui_left", "ui_right")
-	var direction_y := Input.get_axis("ui_up", "ui_down")
 
 	if direction_x:
 		velocity.x = direction_x * SPEED
