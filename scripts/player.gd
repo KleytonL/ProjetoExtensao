37c988743a5a11bffb1f263a0e5090f2b55d2cbc
KleytonL <kleytonl.vieira@gmail.com>
2025-10-10 16:30:01 -0300

    Extendendo paleta de cores e implementando Finite State Machine

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,49 @@
 extends CharacterBody2D
 
+enum States {IDLE, RUN, PUNCH}
 
 const SPEED = 50.0
 const DECELERATION = 200.0
 
-var can_attack: bool = true
-@onready var _animation = $AnimatedSprite2D
+var state: States = States.IDLE : set = set_state
+@onready var _sprite = $AnimatedSprite2D
+@onready var _animation = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta: float) -> void:
 	
 	var direction_x := Input.get_axis("ui_left", "ui_right")
 	var direction_y := Input.get_axis("ui_up", "ui_down")
 
+	if state in [States.IDLE, States.RUN]:
+		if direction_x:
+			velocity.x = direction_x * SPEED
+			_sprite.scale.x = -1 if velocity.x < 0 else 1
+		else:
+			velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
+
+		if direction_y:
+			velocity.y = direction_y * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, DECELERATION * delta)
+
+		if velocity.x != 0 || velocity.y != 0:
+			set_state(States.RUN)
+		else:
+			set_state(States.IDLE)
+
 	if Input.is_action_just_pressed("punch"):
-		_animation.play("punch")
-	
-	if velocity.x != 0 || velocity.y != 0:
-		_animation.play("run")
-	else:
-		_animation.play("idle")
-
-	if direction_x:
-		velocity.x = direction_x * SPEED
-		_animation.flip_h = true if velocity.x < 0 else false 
-	else:
-		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
-
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, DECELERATION * delta)
+		set_state(States.PUNCH)
 
 	move_and_slide()
+
+func set_state(new_state: States) -> void:
+	var previous_state := state
+	state = new_state
+
+	if state == States.IDLE:
+		_animation.travel("idle")
+	elif state == States.RUN:
+		_animation.travel("run")
+	elif state == States.PUNCH:
+		_animation.travel("punch")
+		state = previous_state
