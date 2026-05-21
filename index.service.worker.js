d0eff01df2493758b3d0217ba891ecd7ec429a06
KleytonL <57327779+KleytonL@users.noreply.github.com>
2026-05-21 13:43:25 +0000

    Deploying to gh-pages from @ KleytonL/ProjetoExtensao@f5c68a89b6b9f476481d0d6aaba40385098173cb 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779333245|3102562';
+const CACHE_VERSION = '1779371000|3134959';
 /** @type {string} */
 const CACHE_PREFIX = 'ProjetoExtensao-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
