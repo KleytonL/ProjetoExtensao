af6d37cc5e3daae0b75b8acf8263ab99e53c2fa0
GuilhermeAcioli <147158600+GuilhermeAcioli@users.noreply.github.com>
2026-05-28 00:03:09 +0000

    Deploying to gh-pages from @ KleytonL/ProjetoExtensao@561da76a4651c0eee323fd0188bd1aa1180a5cd7 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779926254|3210733';
+const CACHE_VERSION = '1779926585|3154245';
 /** @type {string} */
 const CACHE_PREFIX = 'ProjetoExtensao-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
